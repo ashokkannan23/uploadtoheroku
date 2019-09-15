15ee9eea9a4bac2bd0b68337526df1c53423ef5e
ashokkannan23 <32809419+ashokkannan23@users.noreply.github.com>
2019-09-15 22:45:52 +0530

    updated lot

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI: 'mongodb://jambu:jambu23@cluster0-shard-00-00-1jfmo.mongodb.net:27017,cluster0-shard-00-01-1jfmo.mongodb.net:27017,cluster0-shard-00-02-1jfmo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'YOUR_OWN_SECRET'
 };
